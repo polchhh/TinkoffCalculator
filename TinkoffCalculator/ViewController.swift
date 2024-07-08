ed838293112003e0105aeed02c3f9735ed8d9b2d
polchhh <golodaevskaya@gmail.com>
2024-07-08 11:45:55 +0300

    2

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else{
+            return
+        }
+        print(buttonText)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
